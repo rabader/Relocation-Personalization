050001c62a444f667c2baac29966bb208287469b
rabader <robert.abader@gmail.com>
2022-11-12 12:25:21 -0500

    concat data by category and use common labels

--- utils/social_explorer_data_clean.py
+++ utils/social_explorer_data_clean.py
@@ -9,12 +9,15 @@ def clean_fbi_crime_raw_data():
     """
     # Get file names of all raw FBI crime data CSVs.
     fbi_file_lst = glob.glob('../data/raw/fbi_crime*.csv')
+
+    fbi_concat_df = pd.DataFrame()
     for fbi_file in fbi_file_lst:
         # Read file as dataframe:
         fbi_df = pd.read_csv(fbi_file, encoding="ISO-8859-1") #Social Explorer uses Western Latin-1 (ISO-8859-1) encoding
 
         # Get year:
         fbi_year = re.findall(r'\d+', [x for x in fbi_df.columns if "Total Population (" in x][0])[0]
+        fbi_df["Year"] = fbi_year
 
         # Begin Cleaning:
         fbi_df = fbi_df.iloc[1:].reset_index(drop=True) #drop first row
@@ -28,16 +31,26 @@ def clean_fbi_crime_raw_data():
                 state_num_dict[state_num] = state_name
             except:
                 pass
-        fbi_df["state_name"] = fbi_df["State"].map(state_num_dict)
+
+        fbi_df.rename(columns={"State":"state_code","County":"county_code"}, inplace=True)
+        fbi_df["State"] = fbi_df["state_code"].map(state_num_dict)
 
         # Get primary key and rename some columns:
-        fbi_df["FIPS_year"] = fbi_df["FIPS"].astype(str)+"_"+str(fbi_year)
+        fbi_df["FIPS_Year"] = fbi_df["FIPS"].astype(str)+"_"+str(fbi_year)
         fbi_df.rename(columns=lambda x: "Total Population" if "Total Population (" in x else x, inplace=True)
         fbi_df.drop(columns="Qualifying Name",inplace=True)
-        fbi_df.rename(columns={"Name of Area":"county_name"}, inplace=True)
+        fbi_df.rename(columns={"Name of Area":"County"}, inplace=True)
 
-        # Save to Processed Data Folder
-        fbi_df.to_csv(f"../data/processed/fbi_crime_{fbi_year}.csv.gz", encoding="utf-8-sig")
+        # Concatenate data
+        fbi_concat_df = pd.concat([fbi_concat_df,fbi_df], ignore_index=True)
+        
+    # Re-order some columns for better readability during exploration:
+    ref_cols = ['FIPS','County','State','FIPS_Year','Year']
+    other_cols = [col for col in fbi_concat_df.columns if col not in ref_cols]
+    fbi_concat_df = fbi_concat_df[ref_cols+other_cols]
+
+    # Save to Processed Data Folder
+    fbi_concat_df.to_csv(f"../data/processed/fbi_crime.csv.gz", encoding="utf-8-sig",index=False)
         
 
 def clean_ucr_crime_raw_data():
@@ -46,6 +59,7 @@ def clean_ucr_crime_raw_data():
     """
     # Get file names of all raw UCR crime data CSVs.
     ucr_file_lst = glob.glob('../data/raw/ucr_crime*.csv')
+    ucr_concat_df = pd.DataFrame()
     for ucr_file in ucr_file_lst:
         # Read file as dataframe:
         ucr_df = pd.read_csv(ucr_file,encoding="ISO-8859-1") #Social Explorer uses Western Latin-1 (ISO-8859-1) encoding
@@ -55,6 +69,7 @@ def clean_ucr_crime_raw_data():
 
         # Get year:
         ucr_year = re.findall(r'\d+', [x for x in ucr_df.columns if "Total Population (" in x][0])[0]
+        ucr_df["Year"] = ucr_year
 
         # Begin Cleaning:
         ucr_df = ucr_df.iloc[1:].reset_index(drop=True) #drop first row
@@ -68,16 +83,26 @@ def clean_ucr_crime_raw_data():
                 state_num_dict[state_num] = state_name
             except:
                 pass
-        ucr_df["state_name"] = ucr_df["State"].map(state_num_dict)
+        
+        ucr_df.rename(columns={"State":"state_code","County":"county_code"}, inplace=True)
+        ucr_df["State"] = ucr_df["state_code"].map(state_num_dict)
 
         # Get primary key and rename some columns:
-        ucr_df["FIPS_year"] = ucr_df["FIPS"].astype(str)+"_"+str(ucr_year)
+        ucr_df["FIPS_Year"] = ucr_df["FIPS"].astype(str)+"_"+str(ucr_year)
         ucr_df.rename(columns=lambda x: "Total Population" if "Total Population (" in x else x, inplace=True)
         ucr_df.drop(columns="Qualifying Name",inplace=True)
-        ucr_df.rename(columns={"Name of Area":"county_name"}, inplace=True)
+        ucr_df.rename(columns={"Name of Area":"County"}, inplace=True)
 
-        # Save to Processed Data Folder
-        ucr_df.to_csv(f"../data/processed/ucr_crime_{ucr_year}.csv.gz",encoding="utf-8-sig")
+        # Concatenate data
+        ucr_concat_df = pd.concat([ucr_concat_df,ucr_df], ignore_index=True)
+    
+    # Re-order some columns for better readability during exploration:
+    ref_cols = ['FIPS','County','State','FIPS_Year','Year']
+    other_cols = [col for col in ucr_concat_df.columns if col not in ref_cols]
+    ucr_concat_df = ucr_concat_df[ref_cols+other_cols]
+
+    # Save to Processed Data Folder
+    ucr_concat_df.to_csv(f"../data/processed/ucr_crime.csv.gz", encoding="utf-8-sig",index=False)
 
 
 def clean_health_raw_data():
@@ -86,6 +111,8 @@ def clean_health_raw_data():
     """
     # Get file names of all raw UCR crime data CSVs.
     health_file_lst = glob.glob('../data/raw/health*.csv')
+
+    health_concat_df = pd.DataFrame()
     for health_file in health_file_lst:
         # Read file as dataframe:
         health_df = pd.read_csv(health_file,encoding="ISO-8859-1") #Social Explorer uses Western Latin-1 (ISO-8859-1) encoding
@@ -95,6 +122,7 @@ def clean_health_raw_data():
 
         # Get year from filename (no indicators in health data):
         health_year = re.findall(r'\d+', health_file)[0]
+        health_df["Year"] = health_year
 
         # Begin Cleaning:
         health_df = health_df.iloc[1:].reset_index(drop=True) #drop first row
@@ -108,20 +136,31 @@ def clean_health_raw_data():
                 state_num_dict[state_num] = state_name
             except:
                 pass
-        health_df["state_name"] = health_df["State"].map(state_num_dict)
+
+        health_df.rename(columns={"State":"state_code","County":"county_code"}, inplace=True)
+        health_df["State"] = health_df["state_code"].map(state_num_dict)
 
         # Get primary key and rename some columns:
-        health_df["FIPS_year"] = health_df["FIPS"].astype(str)+"_"+str(health_year)
+        health_df["FIPS_Year"] = health_df["FIPS"].astype(str)+"_"+str(health_year)
         health_df.rename(columns=lambda x: re.sub("\, .\d.*est.\)",")",x), inplace=True)
         health_df.drop(columns="Qualifying Name",inplace=True)
-        health_df.rename(columns={"Name of Area":"county_name"}, inplace=True)
+        health_df.rename(columns={"Name of Area":"County"}, inplace=True)
 
-        # Save to Processed Data Folder
-        health_df.to_csv(f"../data/processed/health_{health_year}.csv.gz",encoding="utf-8-sig")
+        # Concatenate data
+        health_concat_df = pd.concat([health_concat_df,health_df], ignore_index=True)
+    
+    # Re-order some columns for better readability during exploration:
+    ref_cols = ['FIPS','County','State','FIPS_Year','Year']
+    other_cols = [col for col in health_concat_df.columns if col not in ref_cols]
+    health_concat_df = health_concat_df[ref_cols+other_cols]
+
+    # Save to Processed Data Folder
+    health_concat_df.to_csv(f"../data/processed/health.csv.gz", encoding="utf-8-sig",index=False)
+       
 #%%
-# clean_fbi_crime_raw_data()
-# clean_ucr_crime_raw_data()
-# clean_health_raw_data()
+clean_fbi_crime_raw_data()
+clean_ucr_crime_raw_data()
+clean_health_raw_data()
 
 
 # %%

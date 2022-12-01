26b52abb45b4c400a9550c8b2d35537ce1cde6c5
rabader <robert.abader@gmail.com>
2022-12-01 00:45:15 -0500

    algorithm updated to match latest quiz format

--- utils/county_matcher.py
+++ utils/county_matcher.py
@@ -4,15 +4,19 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import KNeighborsClassifier
 
 
-def county_matcher(county_df,user_df,feature_cols,target_col="FIPS",n_neighbors = 500):
-    
+def county_matcher(county_df,user_ref_df,feature_cols,target_col="FIPS",n_neighbors = 500):
     # Scale user weights:
-    user_weights_array = user_df[["Importance"]].to_numpy().reshape(1,-1)[0]
-    weights_array_max = pd.read_csv("../data/external/max_importance_for_scaling.csv",usecols=["max_importance"]).to_numpy().reshape(1,-1)[0]
+    user_weights_array = user_ref_df[["Importance"]].to_numpy().reshape(1,-1)[0]
+    weights_array_max = user_ref_df[["max_importance"]].to_numpy().reshape(1,-1)[0]
     user_weights_array_scaled = user_weights_array/weights_array_max
     
     # Format user responses for use with knn classifier:
-    user_response_array = user_df[["Response"]].T
+    user_response_array = user_ref_df[["Response"]].T
+    response_array_max = user_ref_df[["max_response"]].to_numpy().reshape(1,-1)[0]
+    user_response_scaled = user_response_array/response_array_max
+
+    # Any remaining NA values will be filled with 0's (i.e. for religion adherancy):
+    county_df = county_df.fillna(value=0)
 
     # Format and scale inputs for classifier:
     y = county_df[target_col].astype(str)
@@ -20,7 +24,6 @@ def county_matcher(county_df,user_df,feature_cols,target_col="FIPS",n_neighbors 
     X_scaler = MinMaxScaler().fit(X)
     X_scaled = X_scaler.transform(X)
     X_scaled_weighted = X_scaled*user_weights_array_scaled #incorporate user-selected importance weights
-    user_response_scaled = X_scaler.transform(user_response_array)
 
     # Create and Fit KNN Classifier:
     neigh = KNeighborsClassifier(n_neighbors=n_neighbors, weights="distance")
@@ -33,4 +36,4 @@ def county_matcher(county_df,user_df,feature_cols,target_col="FIPS",n_neighbors 
     nearest_neighbors = county_df[["FIPS","County","State"]].iloc[np.r_[ind][0]]
     
     return nearest_neighbors
-    # return 
+    # return X,X_scaled,X_scaled_weighted
